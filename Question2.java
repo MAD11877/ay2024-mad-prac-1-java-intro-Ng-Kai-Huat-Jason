6e5d9b450fc266c0146208661228193dcd8648ad
Jason Ng <S10262552@connect.np.edu.sg>
2024-04-15 14:13:15 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
-public class Question2
-{
-  public static void main(String[] args)
-  {
-    /**
-     * Prompts user for height and weight, calculate and print out the BMI of the user
-     * 
-     * Hint: in.nextDouble() to read double
-     */
-     
+public class Question2 {
+  public static void main(String[] args) {
+
     Scanner in = new Scanner(System.in);
+    System.out.println("Enter your height in meters");
+
     double height = in.nextDouble();
+
+    System.out.println("Enter your weight in kg");
+
+    double weight = in.nextDouble();
+
+    in.close();
+
+    double bmi = weight / (height * height);
+    System.out.println("Your BMI is " + String.format("%.1f", bmi));
   }
 }
