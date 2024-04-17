5041466e34d813b8b7774d1c5d69bd96303cde03
Jason Ng <S10262552@connect.np.edu.sg>
2024-04-17 15:46:25 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,60 +1,63 @@
 import java.util.Scanner;
 
-public class Question5
-{
-  public static void main(String[] args)
-  {
+public class Question5 {
+  public static void main(String[] args) {
     /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
+     * Prompt the user for number. This input indicates the number of integers the
+     * user will be entering next.
+     * Print out the mode (highest occurrence) from the set of integers.
+     * e.g.
+     * > 5
+     * > 2
+     * > 4
+     * > 1
+     * > 3
+     * > 4
+     * 4
      * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
+     * e.g.
+     * > 4
+     * > 2
+     * > 2
+     * > 3
+     * > 3
+     * 2
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
-     
     Scanner in = new Scanner(System.in);
 
-    int NoOfInts = in.nextInt();
+    System.out.print("Enter the number of integers: ");
+    int numIntegers = in.nextInt();
 
-    int[] myInts = new int[NoOfInts];
+    int currentMode = 0;
+    int maxCount = 0;
 
-    for (int i = 0; i < NoOfInts; i++) {
-      myInts[i] = in.nextInt();
+    for (int i = 0; i < numIntegers; i++) {
+      int currentInt = in.nextInt();
+      int currentCount = 1;
+
+      for (int j = i + 1; j < numIntegers; j++) {
+        if (currentInt == in.nextInt()) {
+          currentCount++;
+        }
+      }
+
+      if (currentCount > maxCount) {
+        maxCount = currentCount;
+        currentMode = currentInt;
+      }
     }
 
     in.close();
 
-    int mode = 0;
-
-    for (int i = 0; i < NoOfInts; i++) {
-      int num1;
-      int num2;
-
-      num1 = myInts[i];
-      num2 = mode;
-
-      if (num1 > num2) {
-        mode = num1;
-      }
-      else{
-        mode = num2;
-      }
+    if (hasMultipleModes(numIntegers, maxCount)) {
+      System.out.println("The data is multimodal.");
+    } else {
+      System.out.println("The mode of the data is: " + currentMode);
     }
+  }
 
-    System.out.println(mode);
+  private static boolean hasMultipleModes(int numIntegers, int maxCount) {
+    return maxCount > 1;
   }
 }
